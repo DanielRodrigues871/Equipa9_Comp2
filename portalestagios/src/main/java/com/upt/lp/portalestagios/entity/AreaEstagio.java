57a821fbdbe8ebdf7109ceeab3b5716d0af66994
momou <momou@HP9>
2025-11-24 19:27:09 +0000

    pacotes e classes criadas

--- portalestagios/src/main/java/com/upt/lp/portalestagios/entity/AreaEstagio.java
+++ portalestagios/src/main/java/com/upt/lp/portalestagios/entity/AreaEstagio.java
@@ -1,5 +1,74 @@
 package com.upt.lp.portalestagios.entity;
 
+import jakarta.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "area_estagio")
 public class AreaEstagio {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", length = 36, updatable = false, nullable = false)
+    private String id;
+
+    @Column(name = "nome", nullable = false, length = 100)
+    private String nome;
+
+    @Column(name = "descricao", length = 255)
+    private String descricao;
+
+    public AreaEstagio() {}
+
+    public AreaEstagio(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    // Getters e setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof AreaEstagio)) return false;
+        AreaEstagio that = (AreaEstagio) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "AreaEstagio{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                '}';
+    }
 }
