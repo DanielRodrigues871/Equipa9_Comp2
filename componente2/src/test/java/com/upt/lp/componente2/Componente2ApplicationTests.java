daf68ee88a6d59f459d8d4b43bde7acb28ce81a4
gonca <gonca@LAPTOP-OKGA58V4>
2025-11-28 12:39:17 +0000

    .

---
+++ componente2/src/test/java/com/upt/lp/componente2/Componente2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upt.lp.componente2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Componente2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
