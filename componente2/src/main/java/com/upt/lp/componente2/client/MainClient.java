cfccf394181502ef879ae68f7da1267ace584a54
gonca <gonca@LAPTOP-OKGA58V4.Home>
2025-12-01 23:02:38 +0000

    classes de teste

---
+++ componente2/src/main/java/com/upt/lp/componente2/client/MainClient.java
@@ -0,0 +1,8 @@
+package com.upt.lp.componente2.client;
+
+public class MainClient {
+    public static void main(String[] args) {
+        MenuPortalEstagios menu = new MenuPortalEstagios();
+        menu.iniciar();
+    }
+}
